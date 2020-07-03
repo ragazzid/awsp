642deb95e50927d00c312721c5bc90f26480374a
Diego Ragazzi <d@ragazzid.com.br>
2020-07-03 13:04:53 -0300

    v0.0.4

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # awsp
 A simple parser to export sts assume role output to export bash :)
 
+## Usage
+```
+aws sts assume-role --role-arn arn:aws:iam::XXXXXXXXXXXXXXXXXX:role/tmp --role-session-name xtpo | awsp        
+export AWS_ACCESS_KEY_ID=AAAAAAAAAAAAAAAAAA
+export AWS_SECRET_ACCESS_KEY=AAAAAAAAAAAAAAAAAA/AAAAAAAAAAAAAAAAAA
+export AWS_SESSION_TOKEN="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+```
+
+Or, directly to your env variables
+
+```
+eval $(aws sts assume-role --role-arn arn:aws:iam::XXXXXXXXXXXXXXXXXX:role/tmp --role-session-name xtpo | awsp)
+```
+
+Then
+
+```
+aws sts get-caller-identity
+{
+    "Account": "XXXXXXXXXXXXXXXXXX", 
+    "UserId": "YYYYYYYYYYYYYY:xtpo", 
+    "Arn": "arn:aws:sts::XXXXXXXXXXXXXXXXXX:assumed-role/tmp/xtpo"
+}
+
+```
 
 # Build & Send
 ```
--- awsp/__init__.py
+++ awsp/__init__.py
@@ -11,19 +11,15 @@ def remove_spaces(data):
 def print_env_vars(data):
     x = (
         '''
-# You assume the role {}
 export AWS_ACCESS_KEY_ID={}
 export AWS_SECRET_ACCESS_KEY={}
 export AWS_SESSION_TOKEN="{}"
-# It will expire at {}
-# AssumedRoleId {}
         '''.
-            format(data['AssumedRoleUser']['Arn'],
+            format(
                    data['Credentials']['AccessKeyId'],
                    data['Credentials']['SecretAccessKey'],
                    data['Credentials']['SessionToken'],
-                   data['Credentials']['Expiration'],
-                   data['AssumedRoleUser']['AssumedRoleId'])
+                   )
     )
     print(remove_spaces(x))
 
@@ -32,7 +28,7 @@ def run():
     data = remove_spaces(sys.stdin.read())
     try:
         j = json.loads(data)
-        if 'Credentials' in json:
+        if 'Credentials' in j:
             print_env_vars(j)
         else:
             print("I couldn't find any credentials :(\nGot:")
